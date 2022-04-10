46ae12501d004bf76d3799e07d251227a4a3800c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-04-10 14:51:54 +0900

    Delete old code

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,6 @@ use {rayon::prelude::*, rug::Rational};
 
 fn main() {
     let limit = 1_000_000;
-    // let mut curr_sum: Rational = Rational::from((1i64, 1i64));
-    // let mut denominator: i64 = 3;
-    // let mut adding = false;
-    // for _ in 0..limit {
-    //     if adding {
-    //         curr_sum += Rational::from((1i64, denominator));
-    //     } else {
-    //         curr_sum -= Rational::from((1i64, denominator));
-    //     }
-    //     denominator += 2;
-    //     adding = !adding;
-    // }
-    // println!("limit:{} => {}", limit, 4.0 * curr_sum.to_f64());
     let result = (0i64..=limit)
         .into_par_iter()
         .map(|i| if i % 2 == 0 { 2 * i + 1 } else { -2 * i - 1 })
