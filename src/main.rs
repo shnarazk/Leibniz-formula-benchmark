9c445e60533a9849bc9705d9e7b229022c1ba104
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-01-07 08:56:53 +0900

    add feature 'float' 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use {rayon::prelude::*, rug::Rational};
+use rayon::prelude::*;
 
+#[cfg(not(feature = "float"))]
+use rug::Rational;
+
+#[cfg(not(feature = "float"))]
 fn main() {
     let limit = 1_000_000;
     let result = (0i64..=limit)
@@ -8,5 +12,21 @@ fn main() {
         .map(|denominator| Rational::from((1i64, denominator)))
         .sum::<Rational>()
         .to_f64();
+    println!("limit:{} => {}", limit, 4.0 * result);
+}
+
+#[cfg(feature = "float")]
+fn main() {
+    let limit = 20_000_000;
+    let result = (0i64..=limit)
+        .into_par_iter()
+        .map(|i| {
+            if i % 2 == 0 {
+                1.0 / (2 * i + 1) as f64
+            } else {
+                1.0 / (-2 * i - 1) as f64
+            }
+        })
+        .sum::<f64>();
     println!("limit:{} => {}", limit, 4.0 * result);
 }
