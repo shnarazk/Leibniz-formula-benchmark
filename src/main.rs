1f057ed42a6c3e11a32a148b5e8c1e6656f569c6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-26 01:03:52 +0900

    better "float"

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,18 @@ fn main() {
 
 #[cfg(feature = "float")]
 fn main() {
-    let limit = 20_000_000;
+    let limit = 20_000_000 / 2;
     let result = (0i64..=limit)
         .into_par_iter()
         .map(|i| {
-            if i % 2 == 0 {
-                1.0 / (2 * i + 1) as f64
-            } else {
-                1.0 / (-2 * i - 1) as f64
-            }
+            // if i % 2 == 0 {
+            //     1.0 / (2 * i + 1) as f64
+            // } else {
+            //     1.0 / (-2 * i - 1) as f64
+            // }
+            let j = i as f64 * 4.0;
+            // 1.0 / (j + 1) as f64 - 1.0 / (j + 3) as f64
+            2.0 / ((j + 1.0) * (j + 3.0))
         })
         .sum::<f64>();
     println!("f64 limit:{} => {}", limit, 4.0 * result);
